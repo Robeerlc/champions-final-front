b07e631d945f76ade460cf32a76cb18ad296c843
Roberto Lucas <lucascastilloroberto@gmail.com>
2026-05-28 17:47:29 +0200

    PUTO CORS

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,11 +3,15 @@ import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { authInterceptor } from './interceptors/auth-interceptor';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideHttpClient(withInterceptors([authInterceptor])),
-    provideRouter(routes)
+    provideRouter(routes), {
+      provide: LocationStrategy,
+      useClass: HashLocationStrategy
+    }
   ]
 };
