70ab130f2678cb6f5ffd46edda5384fff9245d5a
plushtrap00 <pablete.merejo@gmail.com>
2026-05-28 21:23:48 +0200

    hotfix

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+(window as any).global = window;
+
 import { bootstrapApplication } from '@angular/platform-browser';
 import { registerLocaleData } from '@angular/common';
 import localeEs from '@angular/common/locales/es';
